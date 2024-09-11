97040b725c19b81ab12e7c3cb99a59987fcf4d99
Mateus Ramos <mateusrammos0@gmail.com>
2024-09-11 11:22:36 -0300

    Limite alterado para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -7,6 +7,7 @@ function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+
 }
 
 function exibirMensagemInicial() {
